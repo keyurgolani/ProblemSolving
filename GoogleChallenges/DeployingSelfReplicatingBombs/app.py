c54b4af0d7eee9624c5ffaa6f296a4a41b9cd180
Keyur Golani <keyurrgolani@gmail.com>
2017-02-05 22:48:33 -0800

    Optimization of SelfMultiplying Bombs Problem

--- GoogleChallenges/DeployingSelfReplicatingBombs/app.py
+++ GoogleChallenges/DeployingSelfReplicatingBombs/app.py
@@ -1,25 +1,29 @@
+import datetime
+
 def answer(M, F):
     long_M = long(M)
     long_F = long(F)
     steps = 0
-    while not long_F == long_M == 1:
-        if long_F == 0 or long_M == 0:
+    while long_F != 1 and long_M != 1:
+        if long_F == 0 or long_M == 0 or long_F % long_M == 0 or long_M % long_F == 0 or (long_M % 2 == 0 and long_F % 2 == 0):
             return 'impossible'
         else:
             if long_F < long_M:
-                long_M = long_M - long_F
-                steps += 1
+                increment, long_M = divmod(long_M, long_F)
+                steps += increment
             elif long_F > long_M:
-                long_F = long_F - long_M
-                steps += 1
-            else:
-                return 'impossible'
+                increment, long_F = divmod(long_F, long_M)
+                steps += increment
+    else:
+        steps += abs(long_F - long_M)
     return steps
 
 
 
 def main():
+    start = datetime.datetime.now()
     print answer(*tuple(raw_input().split()))
+    print (datetime.datetime.now() - start).total_seconds()
 
 
 main()
