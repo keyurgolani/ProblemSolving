6ef887b3780b8dd93aad3769795aeaeaf30bf6a0
Keyur Golani <keyurrgolani@gmail.com>
2017-04-19 03:27:12 -0700

    Dynamic Programming Optimization on Longest Common Subsequence Problem

--- Algorithms/LongestCommonSubsequence/app.py
+++ Algorithms/LongestCommonSubsequence/app.py
@@ -1,26 +1,34 @@
+cache_store = {}
+cache_store2 = {}
+
+
 def LCS(A1, A2):
-    if len(A1) == 0 or len(A2) == 0:
-        return []
-    if A1[-1] == A2[-1]:
-        return LCS(A1[:-1], A2[:-1]) + [A1[-1]]
-    else:
-        one = LCS(A1[:-1], A2)
-        two = LCS(A1, A2[:-1])
-        if len(one) > len(two):
-            return one
+    if (A1, A2) not in cache_store.keys():
+        if len(A1) == 0 or len(A2) == 0:
+            cache_store[(A1, A2)] = []
+        elif A1[-1] == A2[-1]:
+            cache_store[(A1, A2)] = LCS(A1[:-1], A2[:-1]) + [A1[-1]]
         else:
-            return two
+            one = LCS(A1[:-1], A2)
+            two = LCS(A1, A2[:-1])
+            if len(one) > len(two):
+                cache_store[(A1, A2)] = one
+            else:
+                cache_store[(A1, A2)] = two
+    return cache_store[(A1, A2)]
 
 
 def LCS2(A1, A2):
-    if len(A1) == 0 or len(A2) == 0:
-        return 0
-    if A1[-1] == A2[-1]:
-        return LCS2(A1[:-1], A2[:-1]) + 1
-    else:
-        one = LCS2(A1[:-1], A2)
-        two = LCS2(A1, A2[:-1])
-        return one if one > two else two
+    if (A1, A2) not in cache_store2.keys():
+        if len(A1) == 0 or len(A2) == 0:
+            cache_store2[(A1, A2)] = 0
+        elif A1[-1] == A2[-1]:
+            cache_store2[(A1, A2)] = LCS2(A1[:-1], A2[:-1]) + 1
+        else:
+            one = LCS2(A1[:-1], A2)
+            two = LCS2(A1, A2[:-1])
+            cache_store2[(A1, A2)] = one if one > two else two
+    return cache_store2[(A1, A2)]
 
 
 def main():
